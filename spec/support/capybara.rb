30afb6e536c9fe77f20aa821599687d18a47d790
hiro1129-2mc <cocom329@gmail.com>
2024-10-15 15:55:00 +0900

    system_spec初期設定

---
+++ spec/support/capybara.rb
@@ -0,0 +1,18 @@
+require 'capybara/rspec'
+require 'selenium-webdriver'
+
+Capybara.register_driver :selenium_chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.javascript_driver = :selenium_chrome
+
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+
+  config.before(:each, type: :system, js: true) do
+    driven_by :selenium_chrome
+  end
+end
