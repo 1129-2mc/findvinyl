da4378d309c36b831592f362f0eff01b0a5dc91c
hiro1129-2mc <cocom329@gmail.com>
2024-10-15 19:32:39 +0900

    system_spec初期設定修正

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,18 +1,8 @@
-require 'capybara/rspec'
-require 'selenium-webdriver'
-
-Capybara.register_driver :selenium_chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
-end
-
-Capybara.javascript_driver = :selenium_chrome
-
-RSpec.configure do |config|
-  config.before(:each, type: :system) do
-    driven_by :rack_test
-  end
-
-  config.before(:each, type: :system, js: true) do
-    driven_by :selenium_chrome
-  end
+Capybara.register_driver :remote_chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('no-sandbox')
+  options.add_argument('headless')
+  options.add_argument('disable-gpu')
+  options.add_argument('window-size=1680,1050')
+  Capybara::Selenium::Driver.new(app, browser: :remote, url: ENV['SELENIUM_DRIVER_URL'], capabilities: options)
 end
