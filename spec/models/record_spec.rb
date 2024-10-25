73d07f186d74cfddc39bd35c43313f1a349d4100
hiro1129-2mc <cocom329@gmail.com>
2024-10-25 20:46:29 +0900

    Fix:Recordモデルスペックのitemsがない場合のテストを削除

--- spec/models/record_spec.rb
+++ spec/models/record_spec.rb
@@ -9,15 +9,6 @@ RSpec.describe Record, type: :model do
     end
   end
 
-  context '関連するitemがない場合' do
-    it '無効であること' do
-      record = build(:record)
-      record.items = []
-      expect(record).to be_invalid
-      expect(record.errors[:items]).to include('を入力してください')
-    end
-  end
-
   context 'コメントが10000文字以下の場合' do
     it '有効であること' do
       record = build(:review, content: 'a' * 10000)
