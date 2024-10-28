cf23b20a4bdff2daf66e5b9b1577a951f091ce53
hiro1129-2mc <cocom329@gmail.com>
2024-10-28 16:13:03 +0900

    Fix:Select2の処理を修正

--- app/javascript/controllers/item_edit_controller.js
+++ app/javascript/controllers/item_edit_controller.js
@@ -11,13 +11,23 @@ export default class extends Controller {
 
   initializeSelect2() {
     $(this.element).find('.select2').each(function() {
-      $(this).select2();
+      // select2が未初期化の場合のみ初期化
+      if (!$(this).data('select2')) {
+        $(this).select2();
+      }
     });
   }
 
   destroySelect2() {
     $(this.element).find('.select2').each(function() {
-      $(this).select2('destroy');
+      try {
+        // select2が初期化されている場合のみ破棄処理を行う
+        if ($(this).data('select2')) {
+          $(this).select2('destroy');
+        }
+      } catch (e) {
+        console.warn('Failed to destroy Select2:', e);
+      }
     });
   }
 }
